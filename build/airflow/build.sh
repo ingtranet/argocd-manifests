e055937f41b0ed659190f157de9d94205808d078
Youngchan Kim <cookieshake.dev@gmail.com>
2026-09-07 20:57:31 +0900

    chore(airflow): upgrade to 3.3.1

--- build/airflow/build.sh
+++ build/airflow/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-AIRFLOW_VERSION=3.2.2
+AIRFLOW_VERSION=3.3.1
 PYTHON_VERSION=3.13
 
-TAG=${AIRFLOW_VERSION}-python${PYTHON_VERSION}
 DATE=$(date +%y%m%d)
 if command -v openssl >/dev/null 2>&1; then
   RANDOM_STR=$(openssl rand -base64 64 | tr -dc 'a-z0-9' | head -c 3)
@@ -13,5 +12,6 @@ fi
 
 docker buildx build --platform linux/arm64,linux/amd64 \
   --push \
-  --build-arg AIRFLOW_TAG=${TAG} \
+  --build-arg AIRFLOW_VERSION="${AIRFLOW_VERSION}" \
+  --build-arg PYTHON_VERSION="${PYTHON_VERSION}" \
   -t docker.io/cookieshake/airflow:${AIRFLOW_VERSION}-${DATE}-${RANDOM_STR} .
